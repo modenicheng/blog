44d2b4c0745880efdd78a162b5afa2f2cf812e65
modenicheng <cheng20070225@outlook.com>
2025-07-26 17:34:06 +0800

    fix(utils)

--- docs/.vuepress/theme/utils/index.ts
+++ docs/.vuepress/theme/utils/index.ts
@@ -1,15 +1,11 @@
 import { computed } from "vue";
 
-const isMobile = computed(() => {
-  // 先判断window对象是否存在（云端编译环境不存在window导致报错）
-  // 加上条件判断避免编译时报错
-  if (window)
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      window.navigator.userAgent
-    );
-
-  return false;
-});
+const isMobile = () => {
+  if (typeof window === "undefined") return false;
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    window.navigator.userAgent
+  );
+};
 
 const getThemeBg = () => {
   const rootStyles = getComputedStyle(document.documentElement);
